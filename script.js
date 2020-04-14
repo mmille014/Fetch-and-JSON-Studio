b9a6c5cb4845e1944995d012e583c64a82a2f9b9
Miller <MMille014@cable.comcast.com>
2020-04-14 10:10:02 -0400

    completed script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+
+    let container = document.getElementById('container');
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {} {
+        return (response.json())
+    }).then(function(json) {
+        json.map(function(article) {
+            container.innerHTML += `            
+            <div class="astronaut">
+                <div class="bio">
+                <h3>${article.firstName} ${article.lastName}</h3>                
+                <ul>
+                    <li>Hours in Space: ${article.hoursInSpace}</li>
+                    <li id='active'>Active: ${article.active}"</li>
+                    <li>Skills: ${article.skills}</li>
+                </ul>
+                </div>
+                <img class="avatar" src="${article.picture}"/>
+            </div>
+            `;
+            let activeStatus = document.getElementById('active');
+            }
+        })
+    }
+})
