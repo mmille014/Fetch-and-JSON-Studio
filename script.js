349b154cc33f63874452f16ac311d5bedec7be9c
Miller <MMille014@cable.comcast.com>
2020-04-14 11:03:07 -0400

    updated changes

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 window.addEventListener("load", function() {
 
     let container = document.getElementById('container');
-    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {} {
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
         return (response.json())
     }).then(function(json) {
-        json.map(function(article) {
+        json.map(function(astronaut) {
             container.innerHTML += `            
             <div class="astronaut">
                 <div class="bio">
-                <h3>${article.firstName} ${article.lastName}</h3>                
+                <h3>${astronaut.firstName} ${astronaut.lastName}</h3>                
                 <ul>
-                    <li>Hours in Space: ${article.hoursInSpace}</li>
-                    <li id='active'>Active: ${article.active}"</li>
-                    <li>Skills: ${article.skills}</li>
+                    <li>Hours in Space: ${astronaut.hoursInSpace}</li>
+                    <li id='active'>Active: ${astronaut.active}"</li>
+                    <li>Skills: ${astronaut.skills}</li>
                 </ul>
                 </div>
-                <img class="avatar" src="${article.picture}"/>
+                <img class="avatar" src="${astronaut.picture}"/>
             </div>
             `;
-            let activeStatus = document.getElementById('active');
-            }
-        })
-    }
-})
+            let activeStatus = document.getElementById('active');            
+        });
+    });
+});
